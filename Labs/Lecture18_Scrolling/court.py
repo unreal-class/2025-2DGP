bd6088dbaa8465b1c51f244f832156e7eb713a19
dustinlee@DUSTIN-ZBOOK <daehyun.lee@gmail.com>
2025-12-01 09:35:44 +0900

    'sync'

---
+++ Labs/Lecture18_Scrolling/court.py
@@ -0,0 +1,78 @@
+from pico2d import load_image, get_canvas_width, get_canvas_height, clamp
+import common
+
+class Court:
+    def __init__(self):
+        self.image = load_image('futsal_court.png')
+        self.cw = get_canvas_width()
+        self.ch = get_canvas_height()
+        # fiil here
+
+
+    def update(self):
+        # fill here
+        pass
+
+    def draw(self):
+        self.image.draw(self.cw // 2, self.ch // 2)
+
+
+class TileCourt:
+    def __init__(self):
+        self.cw = get_canvas_width()
+        self.ch = get_canvas_height()
+
+        # fill here
+
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.window_left = clamp(0, int(common.boy.x) - self.cw // 2, self.w - self.cw - 1)
+        self.window_bottom = clamp(0, int(common.boy.y) - self.ch // 2, self.h - self.ch - 1)
+
+        # fill here
+
+
+class InfiniteCourt:
+
+    def __init__(self):
+        self.image = load_image('futsal_court.png')
+        self.cw = get_canvas_width()
+        self.ch = get_canvas_height()
+        self.w = self.image.w
+        self.h = self.image.h
+
+    def draw(self):
+        self.image.clip_draw_to_origin(self.q3l, self.q3b, self.q3w, self.q3h, 0, 0)                        # quadrant 3
+        self.image.clip_draw_to_origin(self.q2l, self.q2b, self.q2w, self.q2h, 0, self.q3h)                 # quadrant 2
+        self.image.clip_draw_to_origin(self.q4l, self.q4b, self.q4w, self.q4h, self.q3w, 0)                 # quadrant 4
+        self.image.clip_draw_to_origin(self.q1l, self.q1b, self.q1w, self.q1h, self.q3w, self.q3h)          # quadrant 1
+
+    def update(self):
+        # quadrant 3
+        self.q3l = (int(common.boy.x) - self.cw // 2) % self.w
+        self.q3b = (int(common.boy.y) - self.ch // 2) % self.h
+        self.q3w = clamp(0, self.w - self.q3l, self.w)
+        self.q3h = clamp(0, self.h - self.q3b, self.h)
+        # quadrant 2
+        self.q2l = self.q3l
+        self.q2b = 0
+        self.q2w = self.q3w
+        self.q2h = self.ch - self.q3h
+        # quadrand 4
+        self.q4l = 0
+        self.q4b = self.q3b
+        self.q4w = self.cw - self.q3w
+        self.q4h = self.q3h
+        # quadrand 1
+        self.q1l = 0
+        self.q1b = 0
+        self.q1w = self.q4w
+        self.q1h = self.q2h
+
+
+    def handle_event(self, event):
+        pass
+
